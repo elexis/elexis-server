cabac81847e9355991c1280666d00b96a71f5b7b
Marco Descher <marco@descher.at>
2016-01-16 21:01:23 +0100

    Pluggable ShiroSecurityService

--- bundles/es.core/src/info/elexis/server/core/security/ShiroSecurityService.java
+++ bundles/es.core/src/info/elexis/server/core/security/ShiroSecurityService.java
@@ -1,19 +1,12 @@
 package info.elexis.server.core.security;
 
 import java.io.Serializable;
-import java.net.URL;
-import java.nio.file.Path;
-import java.util.Collection;
 import java.util.Optional;
 
-import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.permission.WildcardPermission;
-import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.mgt.SecurityManager;
-import org.apache.shiro.realm.Realm;
-import org.apache.shiro.realm.text.IniRealm;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.session.SessionException;
 import org.apache.shiro.session.mgt.DefaultSessionKey;
@@ -22,34 +15,13 @@ import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import info.elexis.server.core.internal.Activator;
-import info.elexis.server.core.internal.Application;
-
 public class ShiroSecurityService {
 
-	public static final String SESSION_USERID = "session.user.id";
-	public static final String USER_IS_ADMIN = "user.admin";
+	public static final String DEFAULT_REALM = "elexis-server";
 
 	private static Logger log = LoggerFactory.getLogger(ShiroSecurityService.class);
 
 	private static SecurityManager shiroSecurityManager = ShiroAuthorizingRealmsManager.getSecurityManager();
-
-	static {
-//		Path path = Application.getHomeDirectory().resolve("shiro.ini");
-//		Realm realm;
-//		if (path.toFile().exists()) {
-//			realm = new IniRealm(path.toString());
-//			log.info("Initialized security realm from " + path.toString());
-//		} else {
-//			URL resource = Activator.loadResourceFile("shiro.ini");
-//			realm = new IniRealm("url:" + resource.toString());
-//			log.warn("Using fallback security realm. Please provide a configuration in "
-//					+ Application.getHomeDirectory().resolve("shiro.ini").toString());
-//		}
-
-		// TODO add ElexisConnectorSecurityManager
-
-	}
 
 	public static Optional<Serializable> authenticate(String userID, char[] password) {
 		log.info("Authenticating " + userID + " @ " + shiroSecurityManager);
@@ -60,7 +32,6 @@ public class ShiroSecurityService {
 			subject.login(token);
 
 			Session session = subject.getSession();
-			session.setAttribute(SESSION_USERID, userID);
 			log.info("Created session " + session.getId() + " for user " + userID + " @ " + shiroSecurityManager);
 			return Optional.of(session.getId());
 		} catch (AuthenticationException uae) {
