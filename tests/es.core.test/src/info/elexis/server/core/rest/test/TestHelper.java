5c145b161036569fa82709b64f0856ec0425be67
Marco Descher <marco@descher.at>
2016-01-16 21:05:16 +0100

    Cleanup

--- tests/es.core.test/src/info/elexis/server/core/rest/test/TestHelper.java
+++
@@ -1,7 +0,0 @@
-package info.elexis.server.core.rest.test;
-
-public class TestHelper {
-
-	public static final String DBCONNECTION_XML_STRING = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><dbConnection hostName=\"localhost\" port=\"3306\" databaseName=\"elexis\" connectionString=\"jdbc:mysql://localhost:3306/elexis\" username=\"elexis\" password=\"elexis\"><rdbmsType>MYSQL</rdbmsType></dbConnection>";
-
-}
