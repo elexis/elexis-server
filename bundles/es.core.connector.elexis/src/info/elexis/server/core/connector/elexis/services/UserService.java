56cb6cc9b23420d6ceea18e70e17660d42704835
Marco Descher <marco@descher.at>
2016-01-27 11:19:59 +0100

    Refactor Elexis -> UserService, disable Elexis realm

--- bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/services/UserService.java
+++ bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/services/UserService.java
@@ -1,10 +1,28 @@
 package info.elexis.server.core.connector.elexis.services;
 
+import java.util.Collection;
+
+import info.elexis.server.core.connector.elexis.jpa.model.annotated.Role;
 import info.elexis.server.core.connector.elexis.jpa.model.annotated.User;
 
 public class UserService extends AbstractService<User> {
 
-	public UserService() {
+	public static UserService INSTANCE = InstanceHolder.INSTANCE;
+
+	private static final class InstanceHolder {
+		static final UserService INSTANCE = new UserService();
+	}
+
+	private UserService() {
 		super(User.class);
+	}
+
+	public boolean userHasRole(User u, String role) {
+		if (u == null || role == null) {
+			throw new IllegalArgumentException();
+		}
+		Collection<Role> roles = u.getRoles();
+		long count = roles.stream().filter(f -> role.equalsIgnoreCase(f.getId())).count();
+		return (count > 0l);
 	}
 }
