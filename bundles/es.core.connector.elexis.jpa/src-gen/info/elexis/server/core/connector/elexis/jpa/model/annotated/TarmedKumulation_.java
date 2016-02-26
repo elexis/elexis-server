3b305905ba0e2cc78092f104f95f5c7310f714ed
Marco Descher <marco@descher.at>
2016-02-26 15:45:48 +0100

    [4261] Add missing / remove superfluous files

---
+++ bundles/es.core.connector.elexis.jpa/src-gen/info/elexis/server/core/connector/elexis/jpa/model/annotated/TarmedKumulation_.java
@@ -0,0 +1,22 @@
+package info.elexis.server.core.connector.elexis.jpa.model.annotated;
+
+import java.time.LocalDate;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.6.2.v20151217-rNA", date="2016-02-26T15:31:36")
+@StaticMetamodel(TarmedKumulation.class)
+public class TarmedKumulation_ { 
+
+    public static volatile SingularAttribute<TarmedKumulation, String> slaveCode;
+    public static volatile SingularAttribute<TarmedKumulation, String> validSide;
+    public static volatile SingularAttribute<TarmedKumulation, String> masterCode;
+    public static volatile SingularAttribute<TarmedKumulation, String> view;
+    public static volatile SingularAttribute<TarmedKumulation, String> slaveArt;
+    public static volatile SingularAttribute<TarmedKumulation, String> masterArt;
+    public static volatile SingularAttribute<TarmedKumulation, String> typ;
+    public static volatile SingularAttribute<TarmedKumulation, LocalDate> validFrom;
+    public static volatile SingularAttribute<TarmedKumulation, LocalDate> validTo;
+
+}
