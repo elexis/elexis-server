dd98d441996af74b4664e31fe110517c149f76f8
Marco Descher <marco@descher.at>
2016-03-12 21:15:30 +0100

    Docu and prepare-target update

--- bundles/es.core/src/info/elexis/server/core/scheduler/ISchedulerTask.java
+++ bundles/es.core/src/info/elexis/server/core/scheduler/ISchedulerTask.java
@@ -6,7 +6,15 @@ import it.sauronsoftware.cron4j.Task;
 public interface ISchedulerTask {
 
 	public static final SchedulingPattern DAILY_NIGHT = new SchedulingPattern("30 2 * * *");
+	public static final SchedulingPattern EVERY_5_MINUTES = new SchedulingPattern("*/5 * * * *");
 
+	/**
+	 * The {@link SchedulingPattern} to apply
+	 * 
+	 * @see <a href=
+	 *      "http://www.sauronsoftware.it/projects/cron4j/manual.php#p02">www.
+	 *      sauronsoftware.it/projects/cron4j/manual.php#p02</a> Cron4J Scheduling patterns documentation
+	 */
 	public SchedulingPattern getSchedulingPattern();
 
 	public void setTaskId(String taskId);
