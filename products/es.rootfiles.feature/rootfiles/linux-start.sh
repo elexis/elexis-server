38d5ef9883b1203ba0a66d4d222e00a468128378
Marco Descher <marco@descher.at>
2016-03-23 11:00:03 +0100

    Update rootfiles feature

--- products/es.rootfiles.feature/rootfiles/linux-start.sh
+++ products/es.rootfiles.feature/rootfiles/linux-start.sh
@@ -2,4 +2,4 @@
 OSGI_CONSOLE=localhost:7234
 ELS_HOMEDIR=${HOME}/elexis-server/
 nohup ./elexis-server -console ${OSGI_CONSOLE} > ${ELS_HOMEDIR}/logs/console.log 2>&1 & echo $! > elexis-server.pid
-echo "connect to elexis-server osgi console: telnet ${OSGI_CONSOLE}" 
+echo "connect to elexis-server osgi console: telnet ${OSGI_CONSOLE}"
