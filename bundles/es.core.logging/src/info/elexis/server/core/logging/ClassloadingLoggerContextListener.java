c65f69a7d04de4713d7d14c8eda76639dba45a62
Marco Descher <marco@descher.at>
2016-03-30 13:47:25 +0200

    Fix classloading problem for SMTP transmission

---
+++ bundles/es.core.logging/src/info/elexis/server/core/logging/ClassloadingLoggerContextListener.java
@@ -0,0 +1,50 @@
+package info.elexis.server.core.logging;
+
+import javax.activation.CommandMap;
+import javax.activation.MailcapCommandMap;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.LoggerContextListener;
+
+public class ClassloadingLoggerContextListener implements  LoggerContextListener {
+
+	private boolean initialized = false;
+	
+	@Override
+	public boolean isResetResistant() {
+		return false;
+	}
+
+	@Override
+	public void onStart(LoggerContext context) {
+		// add handlers for main mail MIME types
+		
+	}
+
+	@Override
+	public void onReset(LoggerContext context) {
+		System.out.println("onReste");
+	}
+
+	@Override
+	public void onStop(LoggerContext context) {
+	}
+
+	@Override
+	public void onLevelChange(Logger logger, Level level) {
+		if(!initialized) {
+			MailcapCommandMap mc = (MailcapCommandMap)CommandMap.getDefaultCommandMap();
+			mc.addMailcap("text/html;; x-java-content-handler=org.apache.geronimo.mail.handlers.HtmlHandler");
+			mc.addMailcap("text/xml;; x-java-content-handler=org.apache.geronimo.mail.handlers.XMLHandler");
+			mc.addMailcap("text/plain;; x-java-content-handler=org.apache.geronimo.mail.handlers.TextHandler");
+			mc.addMailcap("multipart/*;; x-java-content-handler=org.apache.geronimo.mail.handlers.MultipartHandler; x-java-fallback-entry=true");
+			mc.addMailcap("multipart/mixed;; x-java-content-handler=org.apache.geronimo.mail.handlers.MultipartHandler; x-java-fallback-entry=true");
+			mc.addMailcap("message/rfc822;; x-java-content-handler=org.apache.geronimo.mail.handlers.MessageHandler");
+			CommandMap.setDefaultCommandMap(mc);
+			initialized = true;
+		}
+	}
+
+}
