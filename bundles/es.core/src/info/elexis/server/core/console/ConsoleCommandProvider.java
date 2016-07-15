22839569c0356c3aa2d320373f0d73359e6b41e6
Marco Descher <marco@descher.at>
2016-07-15 08:15:14 +0200

    [4713] Provide command to reconfigure log settings

--- bundles/es.core/src/info/elexis/server/core/console/ConsoleCommandProvider.java
+++ bundles/es.core/src/info/elexis/server/core/console/ConsoleCommandProvider.java
@@ -1,10 +1,17 @@
 package info.elexis.server.core.console;
 
+import java.net.URL;
+
 import org.eclipse.osgi.framework.console.CommandInterpreter;
 import org.eclipse.osgi.framework.console.CommandProvider;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.classic.util.ContextInitializer;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.util.StatusPrinter;
 import info.elexis.server.core.internal.Application;
 import info.elexis.server.core.scheduler.SchedulerService;
 
@@ -37,15 +44,15 @@ public class ConsoleCommandProvider implements CommandProvider {
 	private String system(CommandInterpreter ci) {
 		final String argument = ci.nextArgument();
 		if (argument == null) {
-			return "Usage: es system (halt [force] | restart [force] | status | logTestError)";
+			return "Usage: es system (halt [force] | restart [force] | status | logTestError | reloadLogConfig)";
 		}
-		
+
 		final String fv = ci.nextArgument();
-		boolean force = fv!=null && "force".equals(fv);
-		
+		boolean force = fv != null && "force".equals(fv);
+
 		switch (argument) {
 		case "halt":
-				Application.getInstance().shutdown(force);
+			Application.getInstance().shutdown(force);
 			return "OK";
 		case "restart":
 			Application.getInstance().restart(force);
@@ -53,8 +60,11 @@ public class ConsoleCommandProvider implements CommandProvider {
 		case "status":
 			return Application.getStatus();
 		case "logTestError":
-			LoggerFactory.getLogger(ConsoleCommandProvider.class).error("TEST ERROR");
+			LoggerFactory.getLogger(ConsoleCommandProvider.class).error("TEST {}", "ERROR");
 			return "SENT";
+		case "reloadLogConfig":
+			ConsoleCommandProvider.reloadLogger();
+			return "OK";
 		}
 
 		return getHelp();
@@ -90,6 +100,30 @@ public class ConsoleCommandProvider implements CommandProvider {
 	@Override
 	public String getHelp() {
 		return "Usage: es (system | scheduler)";
+	}
+
+	/**
+	 * Reconfigures the system logger by reloading all log configuration files
+	 * 
+	 * @see <a href=
+	 *      "http://stackoverflow.com/questions/9320133/how-do-i-programmatically
+	 *      -tell-logback-to-reload-configuration">stackoverflow</a>
+	 */
+	public static void reloadLogger() {
+		LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+		ContextInitializer ci = new ContextInitializer(loggerContext);
+		URL url = ci.findURLOfDefaultConfigurationFile(true);
+
+		try {
+			JoranConfigurator configurator = new JoranConfigurator();
+			configurator.setContext(loggerContext);
+			loggerContext.reset();
+			configurator.doConfigure(url);
+		} catch (JoranException je) {
+			// StatusPrinter will handle this
+		}
+		StatusPrinter.printInCaseOfErrorsOrWarnings(loggerContext);
 	}
 
 }
