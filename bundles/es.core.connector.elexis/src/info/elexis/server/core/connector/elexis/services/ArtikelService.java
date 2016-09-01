df7fe7c9d0083e73a0071cd98a81aa144e2424a8
Marco Descher <marco@descher.at>
2016-09-01 09:08:17 +0200

    [4738] ArtikelService Eigenartikel specific methods

--- bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/services/ArtikelService.java
+++ bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/services/ArtikelService.java
@@ -1,6 +1,11 @@
 package info.elexis.server.core.connector.elexis.services;
 
+import java.util.Collections;
+import java.util.List;
+
 import info.elexis.server.core.connector.elexis.jpa.model.annotated.Artikel;
+import info.elexis.server.core.connector.elexis.jpa.model.annotated.Artikel_;
+import info.elexis.server.core.connector.elexis.services.JPAQuery.QUERY;
 
 public class ArtikelService extends AbstractService<Artikel> {
 
@@ -13,4 +18,66 @@ public class ArtikelService extends AbstractService<Artikel> {
 	private ArtikelService() {
 		super(Artikel.class);
 	}
+
+	/**
+	 * 
+	 * @param name
+	 * @param internalName
+	 * @param articleType
+	 *            the article type class, e.g. Artikel#TYP_EIGENARTIKEL
+	 * @return
+	 */
+	public Artikel create(String name, String internalName, String articleType) {
+		em.getTransaction().begin();
+		Artikel art = create(false);
+		art.setName(name);
+		art.setNameIntern(internalName);
+		art.setTyp(articleType);
+		em.getTransaction().commit();
+		return art;
+	}
+
+	/**
+	 * 
+	 * @param eigenartikel
+	 *            an Artikel of {@link Artikel#TYP_EIGENARTIKEL}
+	 * @return all packages registered for an Eigenartikel (including deleted),
+	 *         or an empty list of not applicable
+	 */
+	public static List<Artikel> eigenartikelGetPackagesForProduct(Artikel eigenartikel) {
+		if (!Artikel.TYP_EIGENARTIKEL.equals(eigenartikel.getTyp())) {
+			// not an "Eigenartikel"
+			return Collections.emptyList();
+		}
+
+		JPAQuery<Artikel> qbe = new JPAQuery<Artikel>(Artikel.class, true);
+		qbe.add(Artikel_.extId, QUERY.EQUALS, eigenartikel.getId());
+		return qbe.execute();
+	}
+
+	/**
+	 * Marks an Eigenartikel product and all of its associated children as
+	 * deleted
+	 * 
+	 * @param eigenartikel
+	 */
+	public static void eigenartikelDeleteProductIncludingPackages(Artikel eigenartikel) {
+		List<Artikel> packagesForEigenartikelProduct = eigenartikelGetPackagesForProduct(eigenartikel);
+		for (Artikel artikel : packagesForEigenartikelProduct) {
+			artikel.setDeleted(true);
+			ArtikelService.INSTANCE.write(artikel);
+		}
+		eigenartikel.setDeleted(true);
+	}
+
+	/**
+	 * Determine if an eigenartikel is a product
+	 * 
+	 * @param article
+	 * @return
+	 */
+	public static boolean eigenartikelDetermineIfProduct(Artikel article) {
+		return (article.getExtId() == null || article.getExtId().length() == 0);
+	}
+
 }
