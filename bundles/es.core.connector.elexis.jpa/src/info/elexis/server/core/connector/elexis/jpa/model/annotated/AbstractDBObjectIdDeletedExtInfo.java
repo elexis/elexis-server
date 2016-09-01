bbc3b2e0b4a63787d6f40493a8207842db5892aa
Marco Descher <marco@descher.at>
2016-09-01 08:44:39 +0200

    Support removal of extInfo stored values by value = null

--- bundles/es.core.connector.elexis.jpa/src/info/elexis/server/core/connector/elexis/jpa/model/annotated/AbstractDBObjectIdDeletedExtInfo.java
+++ bundles/es.core.connector.elexis.jpa/src/info/elexis/server/core/connector/elexis/jpa/model/annotated/AbstractDBObjectIdDeletedExtInfo.java
@@ -27,12 +27,27 @@ public abstract class AbstractDBObjectIdDeletedExtInfo extends AbstractDBObjectI
 		this.extInfo = extInfo;
 	}
 
+	/**
+	 * 
+	 * @param key
+	 *            a non-null key value
+	 * @param value
+	 *            the value to store, or <code>null</code> to remove the
+	 *            respective key
+	 */
 	@Transient
 	public void setExtInfoValue(Object key, Object value) {
+		if (key == null) {
+			return;
+		}
 		// we have to create a new object on change
 		// otherwise JPA won't pick-up the change
 		Hashtable<Object, Object> ht = new Hashtable<Object, Object>(getExtInfo());
-		ht.put(key, value);
+		if (value != null) {
+			ht.put(key, value);
+		} else {
+			ht.remove(key);
+		}
 		setExtInfo(ht);
 	}
 	
