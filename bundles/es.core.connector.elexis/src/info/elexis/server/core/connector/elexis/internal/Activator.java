e40a76712f885c60f2639a4985ee2b0877852c61
Marco Descher <marco@descher.at>
2016-11-14 11:19:18 +0100

    [5292] Verify common locale setting, update product

--- bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/internal/Activator.java
+++ bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/internal/Activator.java
@@ -1,16 +1,26 @@
 package info.elexis.server.core.connector.elexis.internal;
 
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import ch.elexis.core.constants.Preferences;
+import info.elexis.server.core.connector.elexis.services.ConfigService;
 import info.elexis.server.core.contrib.ApplicationShutdownRegistrar;
 import info.elexis.server.core.contrib.IApplicationShutdownListener;
 
 public class Activator implements BundleActivator {
 
+	private Logger log = LoggerFactory.getLogger(Activator.class);
+
 	private static BundleContext context;
 	private static IApplicationShutdownListener iasl = new ApplicationShutdownListener();
-	
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -19,6 +29,21 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
+
+		new Timer().schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+				Locale locale = Locale.getDefault();
+				String dbStoredLocale = ConfigService.INSTANCE.get(Preferences.CFG_LOCALE, null);
+				if (dbStoredLocale == null || !locale.toString().equals(dbStoredLocale)) {
+					log.error("System locale [{}] does not match required database locale [{}].", locale.toString(),
+							dbStoredLocale);
+					System.out.println("System locale does not match required database locale!");
+				}
+				// TOOD verify db version?
+			}
+		}, 1000);
 
 		ApplicationShutdownRegistrar.addShutdownListener(iasl);
 	}
@@ -31,7 +56,7 @@ public class Activator implements BundleActivator {
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
 		ApplicationShutdownRegistrar.removeShutdownListener(iasl);
-		
+
 		Activator.context = null;
 	}
 
