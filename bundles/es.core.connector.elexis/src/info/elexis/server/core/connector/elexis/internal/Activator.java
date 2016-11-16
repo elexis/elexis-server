2df8a8bd9c5fbe50b5ba2128f73b03c26ad0a3c1
Marco Descher <marco@descher.at>
2016-11-16 15:18:21 +0100

    Revert "Remove Activator#checkLocale"
    This reverts commit 8652baffd15b92585e85b4dcf3f7ade52f9517cf.

--- bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/internal/Activator.java
+++ bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/internal/Activator.java
@@ -1,10 +1,16 @@
 package info.elexis.server.core.connector.elexis.internal;
 
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.elexis.core.constants.Preferences;
+import info.elexis.server.core.connector.elexis.services.ConfigService;
 import info.elexis.server.core.contrib.ApplicationShutdownRegistrar;
 import info.elexis.server.core.contrib.IApplicationShutdownListener;
 
@@ -24,20 +30,20 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 
-//		new Timer().schedule(new TimerTask() {
-//
-//			@Override
-//			public void run() {
-//				Locale locale = Locale.getDefault();
-//				String dbStoredLocale = ConfigService.INSTANCE.get(Preferences.CFG_LOCALE, null);
-//				if (dbStoredLocale == null || !locale.toString().equals(dbStoredLocale)) {
-//					log.error("System locale [{}] does not match required database locale [{}].", locale.toString(),
-//							dbStoredLocale);
-//					System.out.println("System locale does not match required database locale!");
-//				}
-//				// TOOD verify db version?
-//			}
-//		}, 1000);
+		new Timer().schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+				Locale locale = Locale.getDefault();
+				String dbStoredLocale = ConfigService.INSTANCE.get(Preferences.CFG_LOCALE, null);
+				if (dbStoredLocale == null || !locale.toString().equals(dbStoredLocale)) {
+					log.error("System locale [{}] does not match required database locale [{}].", locale.toString(),
+							dbStoredLocale);
+					System.out.println("System locale does not match required database locale!");
+				}
+				// TOOD verify db version?
+			}
+		}, 1000);
 
 		ApplicationShutdownRegistrar.addShutdownListener(iasl);
 	}
