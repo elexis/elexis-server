b55a8b24d0c224f89a7337a30784d6915da6c534
thomashu <huster@medevit.at>
2016-11-17 21:37:55 +0100

    [5095] removed gitignore for findings jpa ProcedureRequest_

---
+++ bundles/es.findings.fhir.jpa/src-gen/info/elexis/server/findings/fhir/jpa/model/annotated/ProcedureRequest_.java
@@ -0,0 +1,15 @@
+package info.elexis.server.findings.fhir.jpa.model.annotated;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.0.v20160725-rNA")
+@StaticMetamodel(ProcedureRequest.class)
+public class ProcedureRequest_ { 
+
+    public static volatile SingularAttribute<ProcedureRequest, String> patientid;
+    public static volatile SingularAttribute<ProcedureRequest, String> encounterid;
+    public static volatile SingularAttribute<ProcedureRequest, String> content;
+
+}
