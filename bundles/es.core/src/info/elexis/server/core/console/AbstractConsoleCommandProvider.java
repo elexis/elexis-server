d80ff9c821bec6c51736f2be35811e3a4fa9a271
Marco Descher <marco@descher.at>
2016-11-23 15:29:11 +0100

    [5353] Decrease LogLevel for AbstractConsoleCommandProvider

--- bundles/es.core/src/info/elexis/server/core/console/AbstractConsoleCommandProvider.java
+++ bundles/es.core/src/info/elexis/server/core/console/AbstractConsoleCommandProvider.java
@@ -76,7 +76,7 @@ public abstract class AbstractConsoleCommandProvider implements CommandProvider 
 				}
 			} catch (Exception e) {
 				ci.println("Execution error on argument: " + e.getMessage());
-				log.error("Execution error on argument " + argument, e);
+				log.warn("Execution error on argument " + argument, e);
 			}
 		}
 	}
