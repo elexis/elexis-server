7c2a14c96c29160d625cae524e174b291981dc42
Marco Descher <marco@descher.at>
2017-05-29 16:17:43 +0200

    [6079] Add missing metamodel classes

---
+++ bundles/es.core.connector.elexis.jpa/src-gen/info/elexis/server/core/connector/elexis/jpa/model/annotated/Sticker_.java
@@ -0,0 +1,23 @@
+package info.elexis.server.core.connector.elexis.jpa.model.annotated;
+
+import info.elexis.server.core.connector.elexis.jpa.model.annotated.StickerClassLink;
+import info.elexis.server.core.connector.elexis.jpa.model.annotated.StickerObjectLink;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.0.v20160725-rNA")
+@StaticMetamodel(Sticker.class)
+public class Sticker_ { 
+
+    public static volatile SingularAttribute<Sticker, String> image;
+    public static volatile ListAttribute<Sticker, StickerObjectLink> stickerObjectLinks;
+    public static volatile SingularAttribute<Sticker, String> importance;
+    public static volatile SingularAttribute<Sticker, String> background;
+    public static volatile SingularAttribute<Sticker, String> classes;
+    public static volatile SingularAttribute<Sticker, String> name;
+    public static volatile SingularAttribute<Sticker, String> foreground;
+    public static volatile ListAttribute<Sticker, StickerClassLink> stickerClassLinks;
+
+}
