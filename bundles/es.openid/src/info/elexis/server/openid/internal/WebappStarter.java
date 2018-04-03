67fe14c658380f333f754a7a35f3ff91375eaac0
Marco Descher <marco@descher.at>
2018-04-03 08:26:10 +0200

    [10335] Update productive target, fix bundles, features, launches

--- bundles/es.openid/src/info/elexis/server/openid/internal/WebappStarter.java
+++ bundles/es.openid/src/info/elexis/server/openid/internal/WebappStarter.java
@@ -8,10 +8,11 @@ import javax.sql.DataSource;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.jetty.osgi.boot.OSGiServerConstants;
-import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 
@@ -19,6 +20,8 @@ import org.osgi.service.component.annotations.ReferenceCardinality;
 public class WebappStarter {
 
 	private WebAppContext webapp;
+
+	private ServiceRegistration<?> serviceRegistration;
 
 	@Reference(service = DataSource.class, cardinality = ReferenceCardinality.MANDATORY)
 	protected synchronized void bindDataSource(DataSource dataSource) {
@@ -29,13 +32,19 @@ public class WebappStarter {
 	protected void activate() throws IOException {
 		webapp = new WebAppContext();
 		webapp.addBean(new JspStarter(webapp.getServletContext().getContextHandler()));
-		
+		webapp.setThrowUnavailableOnStartupException(true);
+
 		Dictionary<String, String> props = new Hashtable<>();
-		String warFile =FileLocator.toFileURL(Activator.getContext().getBundle().getEntry("lib/openid.war")).getPath();
+		String warFile = FileLocator.toFileURL(Activator.getContext().getBundle().getEntry("lib/openid.war")).getPath();
 		props.put("war", warFile);
 		props.put("contextPath", "/openid");
 		props.put(OSGiServerConstants.MANAGED_JETTY_SERVER_NAME, OSGiServerConstants.MANAGED_JETTY_SERVER_DEFAULT_NAME);
-		Activator.getContext().registerService(ContextHandler.class.getName(), webapp, props);
+		serviceRegistration = Activator.getContext().registerService(WebAppContext.class.getName(), webapp, props);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		serviceRegistration.unregister();
 	}
 
 }
