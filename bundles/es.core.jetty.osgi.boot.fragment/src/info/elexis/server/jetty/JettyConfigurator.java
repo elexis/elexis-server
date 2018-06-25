ee7acdaa513c7ac30a6ef91b6b371113a97314ad
Marco Descher <marco@descher.at>
2018-06-25 13:04:52 +0200

    [11235] Switch to pkcs12 keystore, watch changes on filestore

--- bundles/es.core.jetty.osgi.boot.fragment/src/info/elexis/server/jetty/JettyConfigurator.java
+++ bundles/es.core.jetty.osgi.boot.fragment/src/info/elexis/server/jetty/JettyConfigurator.java
@@ -2,8 +2,11 @@ package info.elexis.server.jetty;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.slf4j.Logger;
@@ -19,10 +22,10 @@ public class JettyConfigurator {
 
 	public static void configureSslContextFactory(String jettyBase, SslContextFactory sslContextFactory) {
 
-		File elexisServerKeyStore = CoreUtil.getHomeDirectory().resolve("elexis-server.keystore").toFile();
+		File elexisServerKeyStore = CoreUtil.getHomeDirectory().resolve("elexis-server.p12").toFile();
 		if (!elexisServerKeyStore.exists()) {
 			try {
-				File defaultKeystore = new File(new URL(jettyBase + "/etc/elexis-server.keystore").getFile());
+				File defaultKeystore = new File(new URL(jettyBase + "/etc/elexis-server.p12").getFile());
 				log.info("Copying default keystore file from [{}] to [{}]", defaultKeystore.getAbsolutePath(),
 						elexisServerKeyStore.getAbsolutePath());
 
@@ -36,6 +39,18 @@ public class JettyConfigurator {
 		sslContextFactory.setTrustStorePath(elexisServerKeyStore.getAbsolutePath());
 		sslContextFactory.setKeyStorePassword(KEYSTORE_PASSWORD);
 		sslContextFactory.setKeyManagerPassword(KEYSTORE_PASSWORD);
+		sslContextFactory.setKeyStoreType("PKCS12");
+		sslContextFactory.setTrustStoreType("PKCS12");
+
+		// watch changes to the keystore and reload
+		// https://danielflower.github.io/2017/04/08/Lets-Encrypt-Certs-with-embedded-Jetty.html
+		Path keystorePath = Paths.get(URI.create(sslContextFactory.getKeyStorePath()));
+		try {
+			FileWatcher.onFileChange(keystorePath,
+					() -> sslContextFactory.reload(scf -> log.info("Reloaded SSL cert")));
+		} catch (IOException e) {
+			log.error("Error initializing File Watcher on [{}]", keystorePath);
+		}
 	}
 
 }
