bb423ea4e6de7f135dec1fd502bb6457f90288c7
Marco Descher <marco@descher.at>
2018-09-07 09:10:38 +0200

    [11519] Provide HAPI FHIR 3.4 bundle

---
+++ bundles/es.hapi.fhir/src/info/elexis/server/hapi/fhir/FhirUtil.java
@@ -0,0 +1,32 @@
+package info.elexis.server.hapi.fhir;
+
+import org.hl7.fhir.instance.model.api.IBaseResource;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
+
+public class FhirUtil {
+
+	private static FhirContext context = FhirContext.forDstu3();
+
+	private static IParser getJsonParser() {
+		return context.newJsonParser();
+	}
+
+	public static IBaseResource getAsResource(String jsonResource) {
+		return getJsonParser().parseResource(jsonResource);
+	}
+
+	public static IGenericClient getGenericClient(String theServerBase) {
+		// Create a logging interceptor
+		LoggingInterceptor loggingInterceptor = new LoggingInterceptor();
+		loggingInterceptor.setLogRequestSummary(true);
+		loggingInterceptor.setLogRequestBody(true);
+
+		IGenericClient client = context.newRestfulGenericClient(theServerBase);
+		client.registerInterceptor(loggingInterceptor);
+		return client;
+	}
+}
