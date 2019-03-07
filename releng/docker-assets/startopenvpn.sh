681e9ea1ddbdd439257fc11928a87ae041741434
Marco Descher <marco@descher.at>
2019-03-07 09:17:16 +0100

    [14871] Consider tun module not loaded

--- releng/docker-assets/startopenvpn.sh
+++ releng/docker-assets/startopenvpn.sh
@@ -9,9 +9,20 @@ if [ ! -f "client.ovpn" ]; then
 	exit 0	
 fi
 
-mkdir -p /dev/net
-if [ ! -c /dev/net/tun ]; then
-    mknod /dev/net/tun c 10 200
-fi
+##
+# From https://www.reddit.com/r/synology/comments/74te0y/howto_deploy_openvpn_on_synology_using_docker/
+##
+# Create the necessary file structure for /dev/net/tun
+ if ( [ ! -c /dev/net/tun ] ); then
+     if ( [ ! -d /dev/net ] ); then
+         mkdir -m 755 /dev/net
+     fi
+     mknod /dev/net/tun c 10 200
+ fi
+
+ if ( !(lsmod | grep -q "^tun\s") ); then
+     echo "TUN kernel module not loaded, OpenVPN will not succeed."
+     exit -1
+ fi
 
 openvpn --config client.ovpn --log /elexis/elexis-server/logs/openvpn.log --script-security 2 --up /letsencrypt.sh --daemon
