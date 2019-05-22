f99d6d50c0c65a15f17ae2818be766d03278c312
Marco Descher <marco@descher.at>
2019-05-22 14:35:52 +0200

    [15150] Update p2 ConsoleCommandProvider

--- bundles/es.core.p2/src/info/elexis/server/core/p2/console/ConsoleCommandProvider.java
+++ bundles/es.core.p2/src/info/elexis/server/core/p2/console/ConsoleCommandProvider.java
@@ -4,56 +4,61 @@ import java.util.Iterator;
 
 import org.eclipse.osgi.framework.console.CommandInterpreter;
 import org.eclipse.osgi.framework.console.CommandProvider;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 
 import ch.elexis.core.console.AbstractConsoleCommandProvider;
+import ch.elexis.core.console.CmdAdvisor;
 import info.elexis.server.core.p2.internal.HTTPServiceHelper;
 import info.elexis.server.core.p2.internal.ProvisioningHelper;
 
 @Component(service = CommandProvider.class, immediate = true)
 public class ConsoleCommandProvider extends AbstractConsoleCommandProvider {
-
-	public void _es_p2(CommandInterpreter ci) {
-		executeCommand("es_p2", ci);
+	
+	@Activate
+	public void activate(){
+		register(this.getClass());
 	}
-
-	public String __executeUpdate() {
+	
+	@CmdAdvisor(description = "feature de-/installation and update repository management")
+	public void _p2(CommandInterpreter ci){
+		executeCommand("p2", ci);
+	}
+	
+	@CmdAdvisor(description = "update all installed features")
+	public String __p2_executeUpdate(){
 		return ProvisioningHelper.updateAllFeatures().getMessage();
 	}
-
-	public String __features() {
-		return getHelp();
+	
+	public String __p2_features_listLocal(){
+		return ProvisioningHelper
+			.getAllInstalledFeatures().stream().map(i -> i.getId() + " (" + i.getVersion() + ") "
+				+ i.getProperty("git-repo-url") + "  " + i.getProperty("git-rev"))
+			.reduce((u, t) -> u + "\n" + t).get();
 	}
-
-	public String __features_listLocal() {
-		return ProvisioningHelper.getAllInstalledFeatures().stream()
-				.map(i -> i.getId() + " (" + i.getVersion() + ") " + i.getProperty("git-repo-url")+"  "+i.getProperty("git-rev"))
-				.reduce((u, t) -> u + "\n" + t).get();
-	}
-
-	public String __features_install(Iterator<String> args) {
+	
+	@CmdAdvisor(description = "install a feature")
+	public String __p2_features_install(Iterator<String> args){
 		if (args.hasNext()) {
 			return ProvisioningHelper.unInstallFeature(args.next(), true);
 		}
 		return missingArgument("featureName");
 	}
-
-	public String __features_uninstall(Iterator<String> args) {
+	
+	@CmdAdvisor(description = "uninstall a feature")
+	public String __p2_features_uninstall(Iterator<String> args){
 		if (args.hasNext()) {
 			return ProvisioningHelper.unInstallFeature(args.next(), false);
 		}
 		return missingArgument("featureName");
 	}
-
-	public String __repo() {
-		return getHelp();
-	}
-
-	public String __repo_list() {
+	
+	@CmdAdvisor(description = "list configured repositories")
+	public String __p2_repo_list(){
 		return HTTPServiceHelper.getRepoInfo(null).toString();
 	}
-
-	public String __repo_add(Iterator<String> args) {
+	
+	public String __p2_repo_add(Iterator<String> args){
 		if (args.hasNext()) {
 			final String url = args.next();
 			String user = null;
@@ -64,17 +69,18 @@ public class ConsoleCommandProvider extends AbstractConsoleCommandProvider {
 			if (args.hasNext()) {
 				password = args.next();
 			}
-			return HTTPServiceHelper.doRepositoryAdd(url, user, password).getStatusInfo().toString();
+			return HTTPServiceHelper.doRepositoryAdd(url, user, password).getStatusInfo()
+				.toString();
 		}
 		return missingArgument("url [user] [password]");
 	}
-
-	public String __repo_remove(Iterator<String> args) {
+	
+	public String __p2_repo_remove(Iterator<String> args){
 		if (args.hasNext()) {
 			final String url = args.next();
 			return HTTPServiceHelper.doRepositoryRemove(url).getStatusInfo().toString();
 		}
 		return missingArgument("url");
 	}
-
+	
 }
