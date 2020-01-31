aeaaf6f0c0a7125fa6936edbafc5382814464e6c
Marco Descher <marco@descher.at>
2020-01-31 10:34:35 +0100

    [18040] ContextService implementation using ThreadLocal

--- bundles/es.core/src/info/elexis/server/core/internal/service/ContextService.java
+++ bundles/es.core/src/info/elexis/server/core/internal/service/ContextService.java
@@ -11,40 +11,44 @@ import ch.elexis.core.services.IContextService;
 @Component
 public class ContextService implements IContextService {
 	
-	private IContext rootContext;
+	private ThreadLocal<IContext> rootContext;
 	
 	@Activate
 	public void activate(){
-		rootContext = new Context();
-		rootContext.setNamed("IS_ELEXIS_SERVER", Boolean.TRUE.toString());
+		rootContext = new ThreadLocal<IContext>() {
+			// global values for all threads
+			@Override
+			protected IContext initialValue(){
+				Context context = new Context();
+				context.setNamed("IS_ELEXIS_SERVER", Boolean.TRUE.toString());
+				return context;
+			}
+		};
 	}
 	
 	@Override
 	public IContext getRootContext(){
-		return rootContext;
+		return rootContext.get();
 	}
 	
 	@Override
 	public Optional<IContext> getNamedContext(String name){
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 	
 	@Override
 	public IContext createNamedContext(String name){
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 	
 	@Override
 	public void releaseContext(String name){
-		// TODO Auto-generated method stub
-		
+		throw new UnsupportedOperationException();	
 	}
 	
 	@Override
 	public void postEvent(String eventTopic, Object object){
-		// TODO Auto-generated method stub	
+		throw new UnsupportedOperationException();	
 	}
 	
 }
