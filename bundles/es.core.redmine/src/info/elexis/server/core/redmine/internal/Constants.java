55960dde24a726614ebd87190e5a7275ea9e3224
Marco Descher <marco@descher.at>
2020-02-05 15:18:30 +0100

    [18039] Refactor Redmine env constant names

--- bundles/es.core.redmine/src/info/elexis/server/core/redmine/internal/Constants.java
+++ bundles/es.core.redmine/src/info/elexis/server/core/redmine/internal/Constants.java
@@ -5,15 +5,13 @@ public class Constants {
 	/**
 	 * Expected environment variable containing the api key to use for the redmine connection
 	 */
-	public static final String ENV_VAR_REDMINE_API_KEY = "redmine.apikey";
+	public static final String ENV_VAR_REDMINE_API_KEY = "REDMINE_APIKEY";
 	
 	/**
 	 * Optional environment variable containing the redmine base url to use, defaults to
 	 * <code>https://redmine.medelexis.ch</code>
 	 */
-	public static final String ENV_VAR_REDMINE_BASE_URL = "redmine.baseurl";
-	
-	
+	public static final String ENV_VAR_REDMINE_BASE_URL = "REDMINE_BASEURL";
 	
 	public static final String DEFAULT_REDMINE_BASE_URL = "https://redmine.medelexis.ch";
 	public static final String DEFAULT_REDMINE_PROJECT = "qfeedback3";
