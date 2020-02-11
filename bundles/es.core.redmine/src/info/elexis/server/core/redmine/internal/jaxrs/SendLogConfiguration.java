dfe3f17e12de22e23e632abb51047fce37a452b9
Marco Descher <marco@descher.at>
2020-02-11 14:07:01 +0100

    [18039] Set defaults for swagger

--- bundles/es.core.redmine/src/info/elexis/server/core/redmine/internal/jaxrs/SendLogConfiguration.java
+++ bundles/es.core.redmine/src/info/elexis/server/core/redmine/internal/jaxrs/SendLogConfiguration.java
@@ -2,11 +2,16 @@ package info.elexis.server.core.redmine.internal.jaxrs;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+import io.swagger.annotations.ApiModelProperty;
+
 @XmlRootElement
 public class SendLogConfiguration {
 	
+	@ApiModelProperty(value = "Issue to append log file to. If 0 generates a new issue.")
 	private Integer issue;
+	@ApiModelProperty(value = "Maximum file size to transmit, defaults to 1 megabyte if 0")
 	private Long maxsize;
+	@ApiModelProperty(value = "The appender to retrieve the log file from", example = "ROLLING")
 	private String appender;
 	
 	public SendLogConfiguration(){
