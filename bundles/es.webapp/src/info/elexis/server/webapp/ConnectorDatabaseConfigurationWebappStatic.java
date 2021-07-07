e5a9e6159ab95580a57bc34ad3dfc42809da9105
Marco Descher <marco@descher.at>
2021-07-07 14:14:05 +0200

    remove logback appenders, es.webapp

--- bundles/es.webapp/src/info/elexis/server/webapp/ConnectorDatabaseConfigurationWebappStatic.java
+++
@@ -1,9 +0,0 @@
-package info.elexis.server.webapp;
-
-import org.osgi.service.component.annotations.Component;
-
-@Component(service = ConnectorDatabaseConfigurationWebappStatic.class, property = {
-		"osgi.http.whiteboard.resource.pattern=/web/elexis-connector/connection/static/*",
-		"osgi.http.whiteboard.resource.prefix=/web/elexis-connector/connection/static" })
-public class ConnectorDatabaseConfigurationWebappStatic {
-}
