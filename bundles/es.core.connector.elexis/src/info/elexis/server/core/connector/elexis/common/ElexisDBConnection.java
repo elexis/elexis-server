bddce451004901a5e448c51c1dbfb99ee0aac84d
Marco Descher <marco@descher.at>
2022-09-08 15:41:48 +0200

    [24552] Remove elexis-connection.xml as connection info source

--- bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/common/ElexisDBConnection.java
+++ bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/common/ElexisDBConnection.java
@@ -6,58 +6,44 @@ import java.util.Optional;
 
 import javax.sql.DataSource;
 
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-
-import ch.elexis.core.common.DBConnection;
 import ch.elexis.core.model.IConfig;
 import ch.elexis.core.services.IModelService;
 import ch.elexis.core.services.holder.CoreModelServiceHolder;
 import ch.elexis.core.utils.OsgiServiceUtil;
-import info.elexis.server.core.connector.elexis.internal.BundleConstants;
 
 public class ElexisDBConnection {
-	
-	public static Optional<DBConnection> getConnection(){
-		return ElexisDBConnectionUtil.getConnection();
-	}
-	
-	public static IStatus getDatabaseInformation(){
-		String statusInfo = getDatabaseInformationString();
-		return new Status(Status.OK, BundleConstants.BUNDLE_ID, statusInfo);
-	}
-	
-	public static String getDatabaseInformationString(){
+
+	public static String getDatabaseInformationString() {
 		IModelService modelService = CoreModelServiceHolder.get();
 		if (modelService == null) {
 			return "No database connection set.";
 		}
-		
+
 		Optional<IConfig> dbVersion = modelService.load("dbversion", IConfig.class);
 		if (!dbVersion.isPresent()) {
 			return "Could not find dbversion entry in config table.";
 		}
-		
+
 		Optional<IConfig> _elVersion = modelService.load("ElexisVersion", IConfig.class);
 		String elVersion = (_elVersion.isPresent()) ? _elVersion.get().getValue() : "unknown";
 		Optional<IConfig> _created = modelService.load("created", IConfig.class);
 		String created = (_created.isPresent()) ? _created.get().getValue() : "unknown";
-		
+
 		String dbMetaData = "";
-		Optional<DataSource> dataSource =
-			OsgiServiceUtil.getService(DataSource.class, "(id=default)");
+		Optional<DataSource> dataSource = OsgiServiceUtil.getService(DataSource.class, "(id=default)");
 		if (dataSource.isPresent()) {
 			try {
 				DatabaseMetaData dbmd = dataSource.get().getConnection().getMetaData();
 				dbMetaData += dbmd.getDatabaseProductName();
 				dbMetaData += " " + dbmd.getDatabaseProductVersion();
 				dbMetaData += " @ " + dbmd.getURL();
-			} catch (SQLException e) {}
+			} catch (SQLException e) {
+			}
 		}
-		
-		String statusInfo = "Elexis " + elVersion + " DBv " + dbVersion.get().getValue()
-			+ ", created " + created + " [" + dbMetaData + "] ";
+
+		String statusInfo = "Elexis " + elVersion + " DBv " + dbVersion.get().getValue() + ", created " + created + " ["
+				+ dbMetaData + "] ";
 		return statusInfo;
-		
+
 	}
 }
--- bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/internal/Activator.java
+++ bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/internal/Activator.java
@@ -1,67 +1,44 @@
 package info.elexis.server.core.connector.elexis.internal;
 
-import java.util.Optional;
-
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.slf4j.LoggerFactory;
 
-import ch.elexis.core.common.DBConnection;
-import ch.elexis.core.services.IElexisDataSource;
-import ch.elexis.core.status.ObjectStatus;
-import ch.elexis.core.utils.OsgiServiceUtil;
-import info.elexis.server.core.connector.elexis.common.ElexisDBConnectionUtil;
 import info.elexis.server.core.contrib.ApplicationShutdownRegistrar;
 import info.elexis.server.core.contrib.IApplicationShutdownListener;
 
 public class Activator implements BundleActivator {
-	
+
 	public static final String BUNDLE_ID = "info.elexis.server.core.connector.elexis";
-	
+
 	private static BundleContext context;
 	private static IApplicationShutdownListener iasl = new ApplicationShutdownListener();
-	
+
 	/*
 	 * (non-Javadoc)
 	 * 
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.
 	 * BundleContext)
 	 */
-	public void start(BundleContext bundleContext) throws Exception{
+	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
-		
-		Optional<IElexisDataSource> datasource =
-			OsgiServiceUtil.getService(IElexisDataSource.class);
-		ObjectStatus currentConnectionStatus = datasource.get().getCurrentConnectionStatus();
-		Optional<DBConnection> configConnection = ElexisDBConnectionUtil.getConnection();
-		
-		if (configConnection.isPresent()) {
-			if (currentConnectionStatus == null) {
-				datasource.get().setDBConnection(configConnection.get());
-			} else {
-				LoggerFactory.getLogger(getClass()).warn(
-					"DB Connection already provided [{}], will NOT initialize via xml file",
-					currentConnectionStatus.getCode());
-			}
-		}
-		
+		// TODO somehow via startup event?
 		ApplicationShutdownRegistrar.addShutdownListener(iasl);
 	}
-	
+
 	/*
 	 * (non-Javadoc)
 	 * 
 	 * @see
 	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
-	public void stop(BundleContext bundleContext) throws Exception{
+	public void stop(BundleContext bundleContext) throws Exception {
 		ApplicationShutdownRegistrar.removeShutdownListener(iasl);
-		
+
 		Activator.context = null;
 	}
-	
-	public static BundleContext getContext(){
+
+	public static BundleContext getContext() {
 		return context;
 	}
-	
+
 }
