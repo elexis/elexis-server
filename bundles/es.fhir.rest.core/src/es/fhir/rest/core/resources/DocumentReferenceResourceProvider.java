65d0d8436737282671880fd7041ddbd1b4da7db3
thomas <huster@medevit.at>
2022-09-20 10:13:23 +0200

    [24550] update document resource provider and tests

--- bundles/es.fhir.rest.core/src/es/fhir/rest/core/resources/DocumentReferenceResourceProvider.java
+++ bundles/es.fhir.rest.core/src/es/fhir/rest/core/resources/DocumentReferenceResourceProvider.java
@@ -1,24 +1,41 @@
 package es.fhir.rest.core.resources;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.DocumentReference;
 import org.hl7.fhir.r4.model.IdType;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.slf4j.LoggerFactory;
 
+import ca.uhn.fhir.rest.annotation.IdParam;
+import ca.uhn.fhir.rest.annotation.Operation;
 import ca.uhn.fhir.rest.annotation.OptionalParam;
 import ca.uhn.fhir.rest.annotation.Search;
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.rest.server.RestfulServer;
+import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
+import ca.uhn.fhir.rest.server.servlet.ServletRequestDetails;
 import ch.elexis.core.findings.IDocumentReference;
 import ch.elexis.core.findings.IFindingsService;
 import ch.elexis.core.findings.migration.IMigratorService;
 import ch.elexis.core.findings.util.fhir.IFhirTransformer;
 import ch.elexis.core.findings.util.fhir.IFhirTransformerRegistry;
+import ch.elexis.core.model.IDocument;
 import ch.elexis.core.model.IPatient;
+import ch.elexis.core.services.IDocumentStore;
 import ch.elexis.core.services.ILocalLockService;
 import ch.elexis.core.services.IModelService;
 
@@ -43,6 +60,9 @@ public class DocumentReferenceResourceProvider
 
 	@Reference
 	private IFindingsService findingsService;
+
+	@Reference
+	private List<IDocumentStore> documentStores;
 
 	public DocumentReferenceResourceProvider() {
 		super(IDocumentReference.class);
@@ -90,5 +110,75 @@ public class DocumentReferenceResourceProvider
 		}
 
 		return null;
+	}
+
+	/**
+	 * $binary-access-read
+	 */
+	@Operation(name = "$binary-access-read", manualResponse = true, idempotent = true)
+	public void binaryAccessRead(@IdParam IIdType theResourceId, ServletRequestDetails theRequestDetails,
+			HttpServletRequest theServletRequest, HttpServletResponse theServletResponse) throws IOException {
+		String idPart = theResourceId.getIdPart();
+		if (idPart != null) {
+			Optional<IDocumentReference> documentReference = findingsService.findById(idPart, IDocumentReference.class);
+			documentReference.ifPresent(ref -> {
+				IDocument document = ref.getDocument();
+				if (document != null) {
+					try {
+						String contentType = document.getMimeType();
+						contentType = StringUtils.defaultIfBlank(contentType, "application/octet-stream");
+
+						theServletResponse.setStatus(200);
+						theServletResponse.setContentType(contentType);
+						theServletResponse.setContentLength((int) document.getContentLength());
+
+						RestfulServer server = theRequestDetails.getServer();
+						server.addHeadersToResponse(theServletResponse);
+
+						IOUtils.copy(document.getContent(), theServletResponse.getOutputStream());
+						theServletResponse.getOutputStream().close();
+					} catch (Exception e) {
+						LoggerFactory.getLogger(DocumentReferenceResourceProvider.class)
+								.error("Error reading document content", e);
+					}
+				}
+			});
+		}
+	}
+
+	/**
+	 * $binary-access-write
+	 */
+	@Operation(name = "$binary-access-write", manualRequest = true, idempotent = false)
+	public IBaseResource binaryAccessWrite(@IdParam IIdType theResourceId, ServletRequestDetails theRequestDetails,
+			HttpServletRequest theServletRequest, HttpServletResponse theServletResponse) throws IOException {
+
+		MethodOutcome outcome = new MethodOutcome();
+		String idPart = theResourceId.getIdPart();
+		if (idPart != null) {
+			Optional<IDocumentReference> documentReference = findingsService.findById(idPart, IDocumentReference.class);
+			documentReference.ifPresent(ref -> {
+				IDocument document = ref.getDocument();
+				if (document != null) {
+					try {
+						IDocumentStore store = documentStores.stream()
+								.filter(s -> s.getId().equals(document.getStoreId())).findFirst().get();
+
+						String requestContentType = theServletRequest.getContentType();
+						if (StringUtils.isBlank(requestContentType)) {
+							throw new InvalidRequestException("No content-target supplied");
+						}
+						store.saveDocument(document, new ByteArrayInputStream(theRequestDetails.loadRequestContents()));
+
+						DocumentReference resource = getTransformer().getFhirObject(ref).orElse(null);
+						outcome.setResource(resource).setId(resource.getIdElement());
+					} catch (Exception e) {
+						LoggerFactory.getLogger(getClass())
+								.error("Error setting binary content of document [" + document + "]", e);
+					}
+				}
+			});
+		}
+		return outcome.getResource();
 	}
 }
--- tests/es.fhir.rest.core.test/src/info/elexis/server/fhir/rest/core/resources/DocumentReferenceTest.java
+++ tests/es.fhir.rest.core.test/src/info/elexis/server/fhir/rest/core/resources/DocumentReferenceTest.java
@@ -1,5 +1,6 @@
 package info.elexis.server.fhir.rest.core.resources;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -10,6 +11,15 @@ import java.sql.SQLException;
 import java.util.Base64;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.hl7.fhir.r4.model.Attachment;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
@@ -20,6 +30,7 @@ import org.hl7.fhir.r4.model.Reference;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import ca.uhn.fhir.rest.api.Constants;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import info.elexis.server.fhir.rest.core.test.AllTests;
@@ -39,7 +50,7 @@ public class DocumentReferenceTest {
 	}
 
 	@Test
-	public void getDocumentReference() {
+	public void getDocumentReference() throws ClientProtocolException, IOException {
 		DocumentReference reference = new DocumentReference();
 		reference.setStatus(DocumentReferenceStatus.CURRENT);
 		reference.setSubject(
@@ -49,13 +60,30 @@ public class DocumentReferenceTest {
 		DocumentReferenceContentComponent content = new DocumentReferenceContentComponent();
 		Attachment attachment = new Attachment();
 		attachment.setTitle("test attachment.txt");
-		attachment.setData(Base64.getEncoder().encode("Test get Text\n2te Zeile üöä!".getBytes()));
 		content.setAttachment(attachment);
 		reference.addContent(content);
 
 		MethodOutcome outcome = client.create().resource(reference).execute();
 		assertNotNull(outcome);
 		assertTrue(outcome.getCreated());
+
+		// upload content
+		DocumentReference refWithContent = null;
+		CloseableHttpClient ourHttpClient = HttpClients.createDefault();
+		HttpPost post = new HttpPost(client.getServerBase() + "/" + ((DocumentReference) outcome.getResource()).getId()
+				+ "/$binary-access-write");
+		post.setEntity(
+				new ByteArrayEntity("Test get Text\n2te Zeile üöä!".getBytes(), ContentType.APPLICATION_OCTET_STREAM));
+		try (CloseableHttpResponse resp = ourHttpClient.execute(post)) {
+			assertEquals(200, resp.getStatusLine().getStatusCode());
+
+			String response = IOUtils.toString(resp.getEntity().getContent(), Constants.CHARSET_UTF8);
+			System.out.println(response);
+			refWithContent = client.getFhirContext().newJsonParser().parseResource(DocumentReference.class,
+					response);
+			byte[] actualBytes = readContent(refWithContent.getContentFirstRep().getAttachment());
+			assertArrayEquals("Test get Text\n2te Zeile üöä!".getBytes(), actualBytes);
+		}
 
 		// search by patient
 		Bundle results = client.search().forResource(DocumentReference.class)
@@ -64,10 +92,11 @@ public class DocumentReferenceTest {
 		assertNotNull(results);
 		List<BundleEntryComponent> entries = results.getEntry();
 		assertFalse(entries.isEmpty());
+
 	}
 
 	@Test
-	public void createDocumentReference() {
+	public void createDocumentReference() throws IOException {
 		DocumentReference reference = new DocumentReference();
 		reference.setStatus(DocumentReferenceStatus.CURRENT);
 		reference.setSubject(
@@ -91,9 +120,20 @@ public class DocumentReferenceTest {
 		assertNotNull(readReference);
 		assertEquals(outcome.getId().getIdPart(), readReference.getIdElement().getIdPart());
 		assertEquals(reference.getSubject().getReference(), readReference.getSubject().getReference());
-		assertEquals(new String(reference.getContent().get(0).getAttachment().getData()),
-				new String(readReference.getContent().get(0).getAttachment().getData()));
+		Attachment readAttachment = readReference.getContent().get(0).getAttachment();
+		assertNotNull(readAttachment);
+		byte[] actualBytes = readContent(readAttachment);
+		assertArrayEquals("Test Text\n2te Zeile üöä!".getBytes(), actualBytes);
+	}
 
-
+	private byte[] readContent(Attachment attachment) throws IOException {
+		byte[] ret = null;
+		CloseableHttpClient ourHttpClient = HttpClients.createDefault();
+		HttpGet get = new HttpGet(client.getServerBase() + "/" + attachment.getUrl());
+		try (CloseableHttpResponse resp = ourHttpClient.execute(get)) {
+			assertEquals(200, resp.getStatusLine().getStatusCode());
+			ret = IOUtils.toByteArray(resp.getEntity().getContent());
+		}
+		return ret;
 	}
 }
