66e8655453399a720171b76725ea99ed3e1eafc9
Marco Descher <marco@descher.at>
2023-01-17 11:05:23 +0100

    [24770] Refactor EventService implementation

--- bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/rest/legacy/EventService.java
+++ bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/rest/legacy/EventService.java
@@ -4,11 +4,8 @@ import javax.ws.rs.core.Response;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventAdmin;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ch.elexis.core.common.ElexisEvent;
@@ -17,22 +14,12 @@ import ch.elexis.core.server.IEventService;
 
 @Component
 public class EventService implements IEventService {
-	
-	private static Logger log = LoggerFactory.getLogger(EventService.class);
-	
-	private static EventAdmin eventAdmin;
-	
-	@Reference(service = EventAdmin.class, cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC, unbind = "unsetEventAdmin")
-	protected synchronized void setEventAdmin(EventAdmin ea){
-		EventService.eventAdmin = ea;
-	}
-	
-	protected synchronized void unsetEventAdmin(EventAdmin ea){
-		EventService.eventAdmin = null;
-	}
-	
+
+	@Reference
+	private EventAdmin eventAdmin;
+
 	@Override
-	public Response postEvent(ElexisEvent elexisEvent){
+	public Response postEvent(ElexisEvent elexisEvent) {
 		if (elexisEvent == null || elexisEvent.getTopic() == null) {
 			return Response.serverError().build();
 		}
@@ -41,12 +28,13 @@ public class EventService implements IEventService {
 			topic = ElexisEventTopics.BASE + topic;
 		}
 		Event event = new Event("remote/" + topic, elexisEvent.getProperties());
-		if (EventService.eventAdmin != null) {
-			EventService.eventAdmin.postEvent(event);
+		if (eventAdmin != null) {
+			eventAdmin.postEvent(event);
 		} else {
-			log.warn("Received post event, but EventAdmin is null");
+			LoggerFactory.getLogger(getClass()).warn("Received post event, but EventAdmin is null");
+			return Response.serverError().build();
 		}
 		return Response.ok().build();
 	}
-	
+
 }
