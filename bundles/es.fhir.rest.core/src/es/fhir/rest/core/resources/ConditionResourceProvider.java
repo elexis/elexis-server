122f4a4b6ea6e2eeadf8301cb780e1475b2c24c7
Marco Descher <marco@descher.at>
2023-02-28 15:17:44 +0100

    [24550] eAU Condition support in ConditionResourceProvider

--- bundles/es.fhir.rest.core/src/es/fhir/rest/core/resources/ConditionResourceProvider.java
+++ bundles/es.fhir.rest.core/src/es/fhir/rest/core/resources/ConditionResourceProvider.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.CodeType;
@@ -20,7 +21,9 @@ import ca.uhn.fhir.rest.annotation.RequiredParam;
 import ca.uhn.fhir.rest.annotation.ResourceParam;
 import ca.uhn.fhir.rest.annotation.Search;
 import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.rest.param.TokenParam;
 import ca.uhn.fhir.rest.server.exceptions.InternalErrorException;
+import ch.elexis.core.fhir.FhirConstants;
 import ch.elexis.core.findings.ICondition;
 import ch.elexis.core.findings.ICondition.ConditionCategory;
 import ch.elexis.core.findings.IFindingsService;
@@ -29,73 +32,87 @@ import ch.elexis.core.findings.util.CodeTypeUtil;
 import ch.elexis.core.findings.util.fhir.IFhirTransformer;
 import ch.elexis.core.findings.util.fhir.IFhirTransformerRegistry;
 import ch.elexis.core.model.IPatient;
+import ch.elexis.core.model.ISickCertificate;
+import ch.elexis.core.model.ModelPackage;
 import ch.elexis.core.services.IModelService;
+import ch.elexis.core.services.IQuery;
+import ch.elexis.core.services.IQuery.COMPARATOR;
 
-@Component
-public class ConditionResourceProvider implements IFhirResourceProvider {
-	
+@Component(service = IFhirResourceProvider.class)
+public class ConditionResourceProvider implements IFhirResourceProvider<Condition, ICondition> {
+
 	@Reference(target = "(" + IModelService.SERVICEMODELNAME + "=ch.elexis.core.model)")
-	private IModelService modelService;
-	
+	private IModelService coreModelService;
+
 	@Reference
 	private IMigratorService migratorService;
-	
+
 	@Reference
 	private IFindingsService findingsService;
-	
+
 	@Reference
 	private IFhirTransformerRegistry transformerRegistry;
-	
+
 	@Override
-	public Class<? extends IBaseResource> getResourceType(){
+	public Class<? extends IBaseResource> getResourceType() {
 		return Condition.class;
 	}
-	
-	@SuppressWarnings("unchecked")
+
 	@Override
-	public IFhirTransformer<Condition, ICondition> getTransformer(){
-		return (IFhirTransformer<Condition, ICondition>) transformerRegistry
-			.getTransformerFor(Condition.class, ICondition.class);
+	public IFhirTransformer<Condition, ICondition> getTransformer() {
+		return (IFhirTransformer<Condition, ICondition>) transformerRegistry.getTransformerFor(Condition.class,
+				ICondition.class);
 	}
-	
+
 	@Read
-	public Condition getResourceById(@IdParam
-	IdType theId){
+	public Condition getResourceById(@IdParam IdType theId) {
 		String idPart = theId.getIdPart();
 		if (idPart != null) {
 			Optional<ICondition> condition = findingsService.findById(idPart, ICondition.class);
 			if (condition.isPresent()) {
 				Optional<Condition> fhirCondition = getTransformer().getFhirObject(condition.get());
 				return fhirCondition.get();
+			} else {
+				Optional<ISickCertificate> sickCertificate = coreModelService.load(idPart, ISickCertificate.class);
+				if (sickCertificate.isPresent()) {
+					return getSickCertificateTransformer().getFhirObject(sickCertificate.get()).get();
+				}
 			}
 		}
 		return null;
 	}
-	
-	@Search()
-	public List<Condition> findCondition(@RequiredParam(name = Condition.SP_SUBJECT)
-	IdType thePatientId, @OptionalParam(name = Condition.SP_CATEGORY)
-	CodeType categoryCode){
+
+	@Search
+	public List<Condition> findCondition(@RequiredParam(name = Condition.SP_SUBJECT) IdType thePatientId,
+			@OptionalParam(name = Condition.SP_CATEGORY) CodeType categoryCode,
+			@OptionalParam(name = Condition.SP_CODE) TokenParam theCode) {
 		if (thePatientId != null && !thePatientId.isEmpty()) {
-			Optional<IPatient> patient =
-				modelService.load(thePatientId.getIdPart(), IPatient.class);
+			Optional<IPatient> patient = coreModelService.load(thePatientId.getIdPart(), IPatient.class);
 			if (patient.isPresent()) {
+
+				if (isAUF(theCode)) {
+					IFhirTransformer<Condition, ISickCertificate> transformer = getSickCertificateTransformer();
+					IQuery<ISickCertificate> query = coreModelService.getQuery(ISickCertificate.class);
+					query.and(ModelPackage.Literals.ISICK_CERTIFICATE__PATIENT, COMPARATOR.EQUALS, patient.get());
+
+					List<Condition> collect = query.execute().stream().map(isc -> transformer.getFhirObject(isc).get())
+							.collect(Collectors.toList());
+					return collect;
+				}
+
 				if (patient.get().isPatient()) {
 					// migrate diagnose condition first
-					migratorService.migratePatientsFindings(thePatientId.getIdPart(),
-						ICondition.class, null);
-					
-					List<ICondition> findings = findingsService
-						.getPatientsFindings(thePatientId.getIdPart(), ICondition.class);
+					migratorService.migratePatientsFindings(thePatientId.getIdPart(), ICondition.class, null);
+
+					List<ICondition> findings = findingsService.getPatientsFindings(thePatientId.getIdPart(),
+							ICondition.class);
 					if (findings != null && !findings.isEmpty()) {
 						List<Condition> ret = new ArrayList<Condition>();
 						for (ICondition iFinding : findings) {
-							if (categoryCode != null
-								&& !isConditionCategory(iFinding, categoryCode)) {
+							if (categoryCode != null && !isConditionCategory(iFinding, categoryCode)) {
 								continue;
 							}
-							Optional<Condition> fhirEncounter =
-								getTransformer().getFhirObject(iFinding);
+							Optional<Condition> fhirEncounter = getTransformer().getFhirObject(iFinding);
 							fhirEncounter.ifPresent(fe -> ret.add(fe));
 						}
 						return ret;
@@ -105,11 +122,38 @@ public class ConditionResourceProvider implements IFhirResourceProvider {
 		}
 		return Collections.emptyList();
 	}
-	
+
+	private boolean isAUF(TokenParam theCode) {
+		if (theCode != null && theCode.getSystem() != null && theCode.getValue() != null) {
+			return FhirConstants.DE_EAU_SYSTEM.equals(theCode.getSystem())
+					&& FhirConstants.DE_EAU_SYSTEM_CODE.equals(theCode.getValue());
+		}
+		return false;
+	}
+
 	@Create
-	public MethodOutcome createCondition(@ResourceParam
-	Condition condition){
+	public MethodOutcome createCondition(@ResourceParam Condition condition) {
 		MethodOutcome outcome = new MethodOutcome();
+
+		if (condition.getCode().hasCoding(FhirConstants.DE_EAU_SYSTEM, FhirConstants.DE_EAU_SYSTEM_CODE)) {
+			// eAUF
+			Optional<ISickCertificate> exists = getSickCertificateTransformer().getLocalObject(condition);
+			if (exists.isPresent()) {
+				outcome.setCreated(false);
+				outcome.setId(new IdType(condition.getId()));
+			} else {
+				Optional<ISickCertificate> created = getSickCertificateTransformer().createLocalObject(condition);
+				if (created.isPresent()) {
+					outcome.setCreated(true);
+					outcome.setId(new IdType(created.get().getId()));
+				} else {
+					throw new InternalErrorException("Creation failed");
+				}
+			}
+
+			return outcome;
+		}
+
 		Optional<ICondition> exists = getTransformer().getLocalObject(condition);
 		if (exists.isPresent()) {
 			outcome.setCreated(false);
@@ -125,12 +169,16 @@ public class ConditionResourceProvider implements IFhirResourceProvider {
 		}
 		return outcome;
 	}
-	
-	private boolean isConditionCategory(ICondition iCondition, CodeType categoryCode){
+
+	private boolean isConditionCategory(ICondition iCondition, CodeType categoryCode) {
 		Optional<String> codeCode = CodeTypeUtil.getCode(categoryCode);
-		
+
 		ConditionCategory category = iCondition.getCategory();
 		return category.name().equalsIgnoreCase(codeCode.orElse(""))
-			|| category.getCode().equalsIgnoreCase(codeCode.orElse(""));
+				|| category.getCode().equalsIgnoreCase(codeCode.orElse(""));
+	}
+
+	private IFhirTransformer<Condition, ISickCertificate> getSickCertificateTransformer() {
+		return transformerRegistry.getTransformerFor(Condition.class, ISickCertificate.class);
 	}
 }
--- tests/es.fhir.rest.core.test/src/info/elexis/server/fhir/rest/core/resources/ConditionTest.java
+++ tests/es.fhir.rest.core.test/src/info/elexis/server/fhir/rest/core/resources/ConditionTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 import org.hl7.fhir.r4.model.Bundle;
@@ -14,15 +16,21 @@ import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.Condition;
+import org.hl7.fhir.r4.model.DateTimeType;
 import org.hl7.fhir.r4.model.Narrative;
 import org.hl7.fhir.r4.model.Patient;
 import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.codesystems.ConditionCategory;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.gclient.ICriterion;
+import ca.uhn.fhir.rest.gclient.TokenClientParam;
+import ch.elexis.core.fhir.FhirConstants;
+import ch.elexis.core.findings.codes.CodingSystem;
 import ch.elexis.core.test.initializer.TestDatabaseInitializer;
 import info.elexis.server.fhir.rest.core.test.AllTests;
 import info.elexis.server.fhir.rest.core.test.FhirUtil;
@@ -34,6 +42,7 @@ public class ConditionTest {
 	@BeforeClass
 	public static void setupClass() throws IOException, SQLException {
 		AllTests.getTestDatabaseInitializer().initializeBehandlung();
+		AllTests.getTestDatabaseInitializer().initializeAUF();
 
 		client = FhirUtil.getGenericClient("http://localhost:8380/fhir");
 		assertNotNull(client);
@@ -42,9 +51,9 @@ public class ConditionTest {
 
 	@Test
 	public void getCondition() {
-		Patient readPatient = client.read().resource(Patient.class).withId(AllTests.getTestDatabaseInitializer().getPatient().getId())
-				.execute();
-				
+		Patient readPatient = client.read().resource(Patient.class)
+				.withId(AllTests.getTestDatabaseInitializer().getPatient().getId()).execute();
+
 		// search by patient
 		Bundle results = client.search().forResource(Condition.class)
 				.where(Condition.SUBJECT.hasId(readPatient.getId())).returnBundle(Bundle.class).execute();
@@ -103,8 +112,8 @@ public class ConditionTest {
 	 */
 	@Test
 	public void getConditionProperties() {
-		Patient readPatient = client.read().resource(Patient.class).withId(AllTests.getTestDatabaseInitializer().getPatient().getId())
-				.execute();
+		Patient readPatient = client.read().resource(Patient.class)
+				.withId(AllTests.getTestDatabaseInitializer().getPatient().getId()).execute();
 
 		// search by patient and category
 		Bundle results = client.search().forResource(Condition.class)
@@ -116,10 +125,62 @@ public class ConditionTest {
 		Condition condition = (Condition) entries.get(0).getResource();
 		assertNotNull(condition);
 
-		assertEquals("Patient/" + AllTests.getTestDatabaseInitializer().getPatient().getId(), condition.getSubject().getReference());
+		assertEquals("Patient/" + AllTests.getTestDatabaseInitializer().getPatient().getId(),
+				condition.getSubject().getReference());
 		assertNotNull(condition.getCategory());
 		assertNotNull(condition.getCategory().get(0).getCoding());
 		assertFalse(condition.getCategory().get(0).getCoding().isEmpty());
 		assertEquals("problem-list-item", condition.getCategory().get(0).getCoding().get(0).getCode());
+	}
+
+	@Test
+	public void findAUFCondition() {
+		Patient readPatient = client.read().resource(Patient.class)
+				.withId(AllTests.getTestDatabaseInitializer().getPatient().getId()).execute();
+
+		ICriterion<TokenClientParam> aufCondition = Condition.CODE.exactly().systemAndCode(FhirConstants.DE_EAU_SYSTEM,
+				FhirConstants.DE_EAU_SYSTEM_CODE);
+		Bundle results = client.search().forResource(Condition.class)
+				.where(Condition.SUBJECT.hasId(readPatient.getId())).where(aufCondition).returnBundle(Bundle.class)
+				.execute();
+		assertNotNull(results);
+		List<BundleEntryComponent> entries = results.getEntry();
+		assertFalse(entries.isEmpty());
+		Condition condition = (Condition) entries.get(0).getResource();
+		assertNotNull(condition);
+		assertEquals("Patient/" + AllTests.getTestDatabaseInitializer().getPatient().getId(),
+				condition.getSubject().getReference());
+
+		List<Coding> coding = condition.getCode().getCoding();
+		assertEquals(2, coding.size());
+
+		Coding codingFirstRep = condition.getStageFirstRep().getType().getCodingFirstRep();
+		assertEquals(CodingSystem.ELEXIS_AUF_DEGREE.getSystem(), codingFirstRep.getSystem());
+		assertEquals("75", codingFirstRep.getCode());
+
+		assertEquals("note", condition.getNote().get(0).getText());
+
+		assertEquals(LocalDate.now().getDayOfMonth(), condition.getOnsetDateTimeType().getDay().intValue());
+	}
+
+	@Test
+	public void loadAUFCondition() {
+		Condition sickCertificate = client.read().resource(Condition.class)
+				.withId(AllTests.getTestDatabaseInitializer().getAUFs().getId()).execute();
+		assertEquals("Patient/" + AllTests.getTestDatabaseInitializer().getPatient().getId(),
+				sickCertificate.getSubject().getReference());
+	}
+
+	@Test
+	@Ignore(value = "unfinished")
+	public void createAUFCondition() {
+		Condition condition = new Condition();
+		condition.getCode().getCoding().add(new Coding(FhirConstants.DE_EAU_SYSTEM, FhirConstants.DE_EAU_SYSTEM_CODE,
+				FhirConstants.DE_EAU_SYSTEM_CODE));
+		condition.getCode().getCoding()
+				.add(new Coding(CodingSystem.ELEXIS_AUF_REASON.getSystem(), "Schwangerschaft", "Schawngerschaft"));
+		condition.setSubject(new Reference("Patient/" + AllTests.getTestDatabaseInitializer().getPatient().getId()));
+		condition.setOnset(new DateTimeType(new Date()));
+
 	}
 }
