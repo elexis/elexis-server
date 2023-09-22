8c1b37f05fa3cd0408aff3665e8970e7a9f3ff00
Marco Descher <marco@descher.at>
2023-09-22 16:15:57 +0200

    [25644] ContextSettingFilter apply user roles, add cache

--- bundles/es.core/src/info/elexis/server/core/security/internal/JaxRsServletConfiguration.java
+++ bundles/es.core/src/info/elexis/server/core/security/internal/JaxRsServletConfiguration.java
@@ -19,6 +19,7 @@ import org.slf4j.LoggerFactory;
 import com.eclipsesource.jaxrs.publisher.ServletConfiguration;
 
 import ch.elexis.core.eenv.IElexisEnvironmentService;
+import ch.elexis.core.services.IAccessControlService;
 import ch.elexis.core.services.IContextService;
 import ch.elexis.core.services.IModelService;
 import ch.elexis.core.utils.OsgiServiceUtil;
@@ -27,56 +28,60 @@ import info.elexis.server.core.servlet.filter.ContextSettingFilter;
 import info.elexis.server.core.servlet.filter.ElexisEnvironmentKeycloakConfigResolver;
 
 /**
- * Register the {@link ShiroFilter} with OSGI Jax RS in order to enforce our security requirements
+ * Register the {@link ShiroFilter} with OSGI Jax RS in order to enforce our
+ * security requirements
  */
 @Component(service = ServletConfiguration.class)
 public class JaxRsServletConfiguration implements ServletConfiguration {
-	
+
 	private static final String SERVICES_BASE_URL = "/services";
 	private static final String OAUTH_CLIENT_POSTFIX = "jaxrs-api";
 	private final String SKIP_PATTERN = SERVICES_BASE_URL + "/(elexis|public)/.*";
-	
+
 	private Logger log = LoggerFactory.getLogger(JaxRsServletConfiguration.class);
-	
+
 	@Reference(target = "(" + IModelService.SERVICEMODELNAME + "=ch.elexis.core.model)")
 	protected IModelService coreModelService;
-	
+
 	@Reference
 	private IContextService contextService;
-	
+
+	@Reference
+	private IAccessControlService accessControlService;
+
 	@Override
-	public HttpContext getHttpContext(HttpService httpService, String rootPath){
-		Thread.currentThread()
-			.setContextClassLoader(JaxRsServletConfiguration.class.getClassLoader());
+	public HttpContext getHttpContext(HttpService httpService, String rootPath) {
+		Thread.currentThread().setContextClassLoader(JaxRsServletConfiguration.class.getClassLoader());
 		ExtendedHttpService extHttpService = (ExtendedHttpService) httpService;
-		
+
 		KeycloakConfigResolver keycloakConfigResolver = null;
-		
+
 		if (!SystemPropertyConstants.isDisableWebSecurity()) {
-			// web security required -  only available via EE / Keycloak setup
-			IElexisEnvironmentService elexisEnvironmentService =
-				OsgiServiceUtil.getService(IElexisEnvironmentService.class).orElse(null);
+			// web security required - only available via EE / Keycloak setup
+			IElexisEnvironmentService elexisEnvironmentService = OsgiServiceUtil
+					.getService(IElexisEnvironmentService.class).orElse(null);
 			if (elexisEnvironmentService == null) {
 				log.error(
-					"Web security enabled, but IElexisEnvironmentService is not available. Aborting JAXRS service setup.");
+						"Web security enabled, but IElexisEnvironmentService is not available. Aborting JAXRS service setup.");
 				throw new IllegalStateException();
 			}
-			
-			keycloakConfigResolver = new ElexisEnvironmentKeycloakConfigResolver(
-				elexisEnvironmentService, OAUTH_CLIENT_POSTFIX);
+
+			keycloakConfigResolver = new ElexisEnvironmentKeycloakConfigResolver(elexisEnvironmentService,
+					OAUTH_CLIENT_POSTFIX);
 		}
-		
+
 		try {
 			if (keycloakConfigResolver != null) {
 				Hashtable<String, String> filterParams = new Hashtable<>();
-				// see https://www.keycloak.org/docs/latest/securing_apps/#_servlet_filter_adapter
+				// see
+				// https://www.keycloak.org/docs/latest/securing_apps/#_servlet_filter_adapter
 				filterParams.put(KeycloakOIDCFilter.SKIP_PATTERN_PARAM, SKIP_PATTERN);
+				extHttpService.registerFilter(SERVICES_BASE_URL + "/*", new KeycloakOIDCFilter(keycloakConfigResolver),
+						filterParams, null);
+
 				extHttpService.registerFilter(SERVICES_BASE_URL + "/*",
-					new KeycloakOIDCFilter(keycloakConfigResolver), filterParams, null);
-				
-				extHttpService.registerFilter(SERVICES_BASE_URL + "/*",
-					new ContextSettingFilter(contextService, coreModelService, SKIP_PATTERN),
-					new Hashtable<>(), null);
+						new ContextSettingFilter(contextService, coreModelService, accessControlService, SKIP_PATTERN),
+						new Hashtable<>(), null);
 			} else {
 				log.error("--- UNPROTECTED JAXRS API ---");
 			}
@@ -85,10 +90,10 @@ public class JaxRsServletConfiguration implements ServletConfiguration {
 		}
 		return null;
 	}
-	
+
 	@Override
-	public Dictionary<String, String> getInitParams(HttpService httpService, String rootPath){
+	public Dictionary<String, String> getInitParams(HttpService httpService, String rootPath) {
 		return null;
 	}
-	
+
 }
