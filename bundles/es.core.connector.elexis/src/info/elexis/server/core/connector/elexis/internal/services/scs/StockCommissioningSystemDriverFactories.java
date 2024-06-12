c7b3e1d64f6ef6cb75e4f82f2170345a3f71fc6e
Marco Descher <marco@descher.at>
2024-06-12 09:51:27 +0200

    [26419] StockCommissioning is privileged

--- bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/internal/services/scs/StockCommissioningSystemDriverFactories.java
+++ bundles/es.core.connector.elexis/src/info/elexis/server/core/connector/elexis/internal/services/scs/StockCommissioningSystemDriverFactories.java
@@ -18,63 +18,67 @@ import org.slf4j.LoggerFactory;
 
 import ch.elexis.core.model.stock.ICommissioningSystemDriverFactory;
 import ch.elexis.core.services.IStockCommissioningSystemService;
+import ch.elexis.core.services.holder.AccessControlServiceHolder;
 import ch.elexis.core.status.StatusUtil;
 
 @Component(service = {})
 public class StockCommissioningSystemDriverFactories {
-	
+
 	@Reference(target = "(role=serverimpl)")
 	private IStockCommissioningSystemService stockCommissioningSystemService;
-	
+
 	private Logger log = LoggerFactory.getLogger(StockCommissioningSystemDriverFactories.class);
-	
-	private static Map<UUID, ICommissioningSystemDriverFactory> driverFactories =
-		new ConcurrentHashMap<UUID, ICommissioningSystemDriverFactory>();
-	
+
+	private static Map<UUID, ICommissioningSystemDriverFactory> driverFactories = new ConcurrentHashMap<UUID, ICommissioningSystemDriverFactory>();
+
 	@Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC, policyOption = ReferencePolicyOption.GREEDY)
-	public void bind(ICommissioningSystemDriverFactory driverFactory){
+	public void bind(ICommissioningSystemDriverFactory driverFactory) {
 		log.debug("Binding " + driverFactory.getClass().getName());
 		driverFactories.put(driverFactory.getIdentification(), driverFactory);
 	}
-	
+
 	@Activate
-	public void activate(){
+	public void activate() {
 		log.trace("Initializing stock commissioning systems.");
-		
-		List<UUID> allDriverUuids = getAllDriverUuids();
-		for (UUID uuid : allDriverUuids) {
-			log.info("Initializing stock commissioning systems for driver id [{}]",
-				StockCommissioningSystemDriverFactories.getInfoStringForDriver(uuid, true));
-			IStatus status = stockCommissioningSystemService.initializeInstancesUsingDriver(uuid);
+
+		AccessControlServiceHolder.get().doPrivileged(() -> {
+			List<UUID> allDriverUuids = getAllDriverUuids();
+			for (UUID uuid : allDriverUuids) {
+				log.info("Initializing stock commissioning systems for driver id [{}]",
+						StockCommissioningSystemDriverFactories.getInfoStringForDriver(uuid, true));
+				IStatus status = stockCommissioningSystemService.initializeInstancesUsingDriver(uuid);
+				if (!status.isOK()) {
+					StatusUtil.logStatus(log, status, true);
+				}
+			}
+		});
+	}
+
+	public void unbind(ICommissioningSystemDriverFactory driverFactory) {
+		log.debug("Unbinding " + driverFactory.getClass().getName());
+
+		AccessControlServiceHolder.get().doPrivileged(() -> {
+			log.info("Shutting down stock commissioning systems for driver id [{}]",
+					driverFactory.getIdentification().toString());
+			IStatus status = stockCommissioningSystemService
+					.shutdownInstancesUsingDriver(driverFactory.getIdentification());
 			if (!status.isOK()) {
 				StatusUtil.logStatus(log, status, true);
 			}
-		}
-	}
-	
-	public void unbind(ICommissioningSystemDriverFactory driverFactory){
-		log.debug("Unbinding " + driverFactory.getClass().getName());
-		
-		log.info("Shutting down stock commissioning systems for driver id [{}]",
-			driverFactory.getIdentification().toString());
-		IStatus status = stockCommissioningSystemService
-			.shutdownInstancesUsingDriver(driverFactory.getIdentification());
-		if (!status.isOK()) {
-			StatusUtil.logStatus(log, status, true);
-		}
-		
+		});
+
 		driverFactories.remove(driverFactory.getIdentification());
 	}
-	
-	public static ICommissioningSystemDriverFactory getDriverFactory(UUID driver){
+
+	public static ICommissioningSystemDriverFactory getDriverFactory(UUID driver) {
 		return driverFactories.get(driver);
 	}
-	
-	public static List<UUID> getAllDriverUuids(){
+
+	public static List<UUID> getAllDriverUuids() {
 		return new ArrayList<UUID>(driverFactories.keySet());
 	}
-	
-	public static String getInfoStringForDriver(UUID driverUuid, boolean extended){
+
+	public static String getInfoStringForDriver(UUID driverUuid, boolean extended) {
 		ICommissioningSystemDriverFactory icsdf = driverFactories.get(driverUuid);
 		if (icsdf != null) {
 			if (extended) {
