fa475682a3445a28582197b0e93f61c9bc48c32c
thuster <huster@medevit.at>
2025-07-01 10:46:29 +0200

    [27160] use absolute url if available for smart on fhir oauth-uris

--- bundles/es.fhir.rest.core/src/es/fhir/rest/core/resources/ServerCapabilityStatementProvider.java
+++ bundles/es.fhir.rest.core/src/es/fhir/rest/core/resources/ServerCapabilityStatementProvider.java
@@ -1,5 +1,6 @@
 package es.fhir.rest.core.resources;
 
+import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.r4.model.CapabilityStatement;
 import org.hl7.fhir.r4.model.CapabilityStatement.CapabilityStatementRestSecurityComponent;
 import org.hl7.fhir.r4.model.CodeableConcept;
@@ -9,6 +10,8 @@ import org.hl7.fhir.r4.model.UriType;
 
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.server.RestfulServer;
+import ch.elexis.core.eenv.IElexisEnvironmentService;
+import ch.elexis.core.utils.OsgiServiceUtil;
 import jakarta.servlet.http.HttpServletRequest;
 
 public class ServerCapabilityStatementProvider
@@ -38,37 +41,49 @@ public class ServerCapabilityStatementProvider
 	}
 
 	private CapabilityStatementRestSecurityComponent getSmartOnFhirCapabilityStatementRestSecurityComponent(
-		HttpServletRequest theRequest){
-		CapabilityStatementRestSecurityComponent csrsc =
-			new CapabilityStatementRestSecurityComponent();
-		
+			HttpServletRequest theRequest) {
+		CapabilityStatementRestSecurityComponent csrsc = new CapabilityStatementRestSecurityComponent();
+
 		CodeableConcept smartOnFhirConcept = new CodeableConcept();
 		Coding coding = new Coding();
 		coding.setSystem("http://hl7.org/fhir/restful-security-service");
 		coding.setCode("SMART-on-FHIR");
 		smartOnFhirConcept.addCoding(coding);
-		smartOnFhirConcept
-			.setText("OAuth2 using SMART-on-FHIR profile (see http://docs.smarthealthit.org)");
-		
+		smartOnFhirConcept.setText("OAuth2 using SMART-on-FHIR profile (see http://docs.smarthealthit.org)");
+
 		Extension oauthExtension = new Extension();
-		oauthExtension
-			.setUrl("http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris");
-		
+		oauthExtension.setUrl("http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris");
+
+		String eeKeycloakRealmEndpoint = StringUtils.EMPTY;
+		IElexisEnvironmentService eeService = OsgiServiceUtil.getService(IElexisEnvironmentService.class).orElse(null);
+		if (eeService != null) {
+			eeKeycloakRealmEndpoint = eeService.getKeycloakRealmEndpoint();
+			OsgiServiceUtil.ungetService(eeService);
+		}
+
 		Extension oauthTokenExtension = new Extension();
 		oauthTokenExtension.setUrl("token");
-		oauthTokenExtension
-				.setValue(new UriType("/keycloak/auth/realms/ElexisEnvironment/protocol/openid-connect/token"));
+		if (StringUtils.isNotBlank(eeKeycloakRealmEndpoint)) {
+			oauthTokenExtension.setValue(new UriType(eeKeycloakRealmEndpoint + "protocol/openid-connect/token"));
+		} else {
+			oauthTokenExtension
+					.setValue(new UriType("/keycloak/auth/realms/ElexisEnvironment/protocol/openid-connect/token"));
+		}
 		oauthExtension.getExtension().add(oauthTokenExtension);
-		
+
 		Extension authTokenExtension = new Extension();
 		authTokenExtension.setUrl("authorize");
-		authTokenExtension
-				.setValue(new UriType("/keycloak/auth/realms/ElexisEnvironment/protocol/openid-connect/auth"));
+		if (StringUtils.isNotBlank(eeKeycloakRealmEndpoint)) {
+			authTokenExtension.setValue(new UriType(eeKeycloakRealmEndpoint + "protocol/openid-connect/auth"));
+		} else {
+			authTokenExtension
+					.setValue(new UriType("/keycloak/auth/realms/ElexisEnvironment/protocol/openid-connect/auth"));
+		}
 		oauthExtension.getExtension().add(authTokenExtension);
 
 		csrsc.getService().add(smartOnFhirConcept);
 		csrsc.getExtension().add(oauthExtension);
-		
+
 		return csrsc;
 	}
 	
