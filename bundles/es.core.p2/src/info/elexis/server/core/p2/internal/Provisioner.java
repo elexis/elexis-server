73760d489a26fb03781e5c860afe466f0f6d98dc
Marco Descher <marco@descher.at>
2025-10-23 19:41:07 +0200

    [27434] Try to fix p2 service

--- bundles/es.core.p2/src/info/elexis/server/core/p2/internal/Provisioner.java
+++ bundles/es.core.p2/src/info/elexis/server/core/p2/internal/Provisioner.java
@@ -18,6 +18,7 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.equinox.p2.core.IProvisioningAgent;
 import org.eclipse.equinox.p2.core.IProvisioningAgentProvider;
 import org.eclipse.equinox.p2.core.ProvisionException;
+import org.eclipse.equinox.p2.core.spi.IAgentServiceFactory;
 import org.eclipse.equinox.p2.engine.IProfile;
 import org.eclipse.equinox.p2.engine.IProfileRegistry;
 import org.eclipse.equinox.p2.metadata.IInstallableUnit;
@@ -68,6 +69,11 @@ public class Provisioner implements IProvisioner {
 	@Reference(service = IProvisioningAgentProvider.class, cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC, unbind = "unsetAgentProvider")
 	protected void setAgentProvider(IProvisioningAgentProvider agentProvider) {
 		this.agentProvider = agentProvider;
+	}
+
+	@Reference(target = "(p2.agent.servicename=org.eclipse.equinox.p2.repository.metadata.IMetadataRepositoryManager)")
+	protected void setMetadataRepositoryManager(IAgentServiceFactory serviceFactory) {
+		// is necessary to have the service loaded on startup
 	}
 
 	protected void unsetAgentProvider(IProvisioningAgentProvider agentProvider) {
@@ -225,16 +231,16 @@ public class Provisioner implements IProvisioner {
 
 	@Override
 	public Collection<IInstallableUnit> getAllAvailableFeatures(IProgressMonitor monitor) {
-		if(monitor == null) {
+		if (monitor == null) {
 			monitor = new NullProgressMonitor();
 		}
-		IQueryResult<IInstallableUnit> result =
-			metadataRepositoryManager.query(QueryUtil.createIUGroupQuery(), monitor);
+		IQueryResult<IInstallableUnit> result = metadataRepositoryManager.query(QueryUtil.createIUGroupQuery(),
+				monitor);
 		return result.toSet();
 	}
 
 	@Override
-	public IInstallableUnit getFeatureInAllAvailableFeatures(IProgressMonitor monitor, String id){
+	public IInstallableUnit getFeatureInAllAvailableFeatures(IProgressMonitor monitor, String id) {
 		Collection<IInstallableUnit> allAvailableFeatures = getAllAvailableFeatures(monitor);
 		for (IInstallableUnit iu : allAvailableFeatures) {
 			if (iu.getId().equalsIgnoreCase(id))
@@ -286,9 +292,9 @@ public class Provisioner implements IProvisioner {
 
 		registerHttpAuthentication(location, username, password);
 	}
-	
+
 	@Override
-	public IStatus loadRepository(IProgressMonitor monitor, URI location){
+	public IStatus loadRepository(IProgressMonitor monitor, URI location) {
 		try {
 			metadataRepositoryManager.loadRepository(location, monitor);
 			return Status.OK_STATUS;
